550d5bca53955f554aa4a87e5f08eaaf53a31cd0
mar5k <mark.petrosyan.y@tumo.org>
2019-05-22 23:35:37 +0400

    randomeventsseasons

--- modules/random.js
+++ modules/random.js
@@ -1,11 +1,19 @@
-function random(items) {
-    var item;
-    if (Array.isArray(items)) {
-        item = items[Math.floor(Math.random() * items.length)];
-    } else if (typeof (items) == 'number') {
-        item = Math.floor(Math.random() * items);
+// function random(items) {
+//     var item;
+//     if (Array.isArray(items)) {
+//         item = items[Math.floor(Math.random() * items.length)];
+//     } else if (typeof (items) == 'number') {
+//         item = Math.floor(Math.random() * items);
+//     }
+//     return item;
+// }
+
+// module.exports = random;
+
+var random = (item) => {
+    if (typeof (item) != 'string') {
+        return item.length ? item[Math.floor(Math.random() * item.length)] : Math.floor(Math.random() * item);
     }
-    return item;
 }
 
 module.exports = random;
